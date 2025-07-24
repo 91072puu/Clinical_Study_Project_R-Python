be882ccf71a323d7567139149fcf642411b9a4e5
91072puu <46866108+91072puu@users.noreply.github.com>
2025-07-24 21:08:27 +0300

    adsl update

---
+++ scripts/random_note.py
@@ -0,0 +1,41 @@
+# ---------------------------------------------------
+#  Random notes for Data checks
+# ---------------------------------------------------
+
+# ### Data checks
+#Data checks
+#print(ADSL.head())
+#print(ADSL.columns)
+#list(dm.columns)
+
+
+# Data types
+#print(ADSL.dtypes)
+
+# Check missing values
+#print(adsl.isnull().sum())
+
+# Check unique values
+#print(ds_disp['DSDECOD'].unique())
+#print(adsl['RACE'].unique())
+
+# Check summary stats
+#print(adsl.describe())
+
+# Check value counts by categories
+#print(adsl['AGEGRP1'].value_counts())
+
+#Descriptive stat.
+#print(dm['AGE'].describe())
+
+#histogram
+
+#dm['AGE'].hist(bins=20)
+#plt.xlabel('Age')
+#plt.ylabel('Count')
+#plt.title('Age Distribution')
+#plt.show()
+
+
+
+
